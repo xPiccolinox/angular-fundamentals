43e2d2249025c32f91152f9a4abd3cca80173fe0
Jakub Sobór <kupkeu@gmail.com>
2022-12-11 19:00:21 +0100

    first commit

---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { HttpModule } from '@angular/http'
+
+// Containers
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component'
+
+// Components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component'
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component'
+
+// Service
+import { PassengerDashboardService } from './passenger-dashboard.service'
+
+@NgModule({
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpModule
+  ],
+  exports: [
+    PassengerDashboardComponent
+  ],
+  providers: [
+    PassengerDashboardService
+  ]
+})
+
+export class PassengerDashboardModule {}
---
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Passenger } from './models/passenger.interface';
+
+const PASSENGER_API: string = '/api/passengers';
+
+@Injectable()
+export class PassengerDashboardService {
+  constructor(private http: Http) {}
+
+  getPassengers(): Observable<Passenger[]> {
+    return this.http
+      .get(PASSENGER_API)
+      .map((response: Response) => response.json());
+  }
+
+}
