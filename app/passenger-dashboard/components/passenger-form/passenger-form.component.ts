a10f9b5301c4517e4b1349c0f10d5c184dee0753
Jakub Sobór <kupkeu@gmail.com>
2022-12-24 17:50:29 +0100

    Feat: Baggage interface & form

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core'
+
 import { Passenger } from '../../models/passenger.interface'
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
   selector: 'passenger-form',
@@ -12,16 +14,52 @@ import { Passenger } from '../../models/passenger.interface'
       <input
         type="text"
         name="fullname"
-        [ngModel]="detail?.fullname"
-      >
+        [ngModel]="detail?.fullname">
     </div>
     <div>
       Passenger ID:
       <input
         type="number"
         name="id"
-        [ngModel]="detail?.id"
-      >
+        [ngModel]="detail?.id">
+    </div>
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          name="checkedIn"
+          [ngModel]="detail?.checkedIn"
+          (ngModelChange)="toogleCheckIn($event)">
+      </label>
+    </div>
+    <div *ngIf="form.value.checkedIn">
+      Check in Date:
+      <input
+        type="number"
+        name="checkInDate"
+        [ngModel]="detail?.checkInDate">
+    </div>
+    <div>
+      Luggage:
+      <select
+        name="baggage"
+        [ngModel]="detail?.baggage">
+        <option
+          *ngFor="let item of baggage"
+          [value]="item.key"
+          [selected]="item.key === detail?.baggage">
+          {{ item.value }}
+        </option>
+      </select>
+      <select
+        name="baggage"
+        [ngModel]="detail?.baggage">
+        <option
+          *ngFor="let item of baggage"
+          [ngValue]="item.key">
+          {{ item.value }}
+        </option>
+      </select>
     </div>
     {{ form.value | json }}
   </form>
@@ -30,4 +68,24 @@ import { Passenger } from '../../models/passenger.interface'
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+  baggage: Baggage[] = [{
+    key: 'none',
+    value: 'No baggage'
+  },{
+    key: 'hand-only',
+    value: 'Hand baggage'
+  },{
+    key: 'hold-only',
+    value: 'Hold baggage'
+  },{
+    key: 'hand-hold',
+    value: 'Hand and hold baggage'
+  },
+]
+
+  toogleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkInDate = Date.now()
+    }
+  }
 }
